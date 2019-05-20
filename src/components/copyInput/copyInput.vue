6cfa424127c90709d9c6b68898d7c2978777664c
wuxianqiang <2631640352@qq.com>
2019-05-20 14:11:34 +0800

    修改文件

---
+++ src/components/copyInput/copyInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="copy-input">
+    <input
+      type="text"
+      class="copy-input__text"
+      ref="input"
+      readonly
+      v-model="link">
+      <i class="el-icon-document copy-input__icon" @click="handleClick" @mouseleave="handleLeave"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: '点击复制',
+      link: 'https://github.com/wuxianqiang/vue-jwt.git'
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$refs.input.select()
+      document.execCommand('copy')
+      this.$prompt.success('复制成功 ！')
+    },
+    handleLeave () {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.copy-input {
+  display: inline-block;
+  border: 1px solid #d1d5da;
+  border-radius: 5px;
+  width: 318px;
+  height: 28px;
+  display: flex;
+  &__text {
+    flex: 1;
+    height: 100%;
+    outline: none;
+    border: none;
+    background: none;
+    padding: 0 5px;
+    border-right: 1px solid #d1d5da;
+  }
+  &__icon {
+    width: 36px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/components/icon/icon.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg :style="cls" aria-hidden="true">
+    <use :xlink:href="href" ></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: String,
+      default: '30'
+    },
+    height: {
+      type: String,
+      default: '30'
+    },
+    fill: {
+      type: String,
+      default: '#8d8ca0'
+    }
+  },
+
+  computed: {
+    cls () {
+      return {
+        width: `${this.width}px`,
+        height: `${this.height}px`,
+        fill: this.fill
+      }
+    }
+  }
+}
+</script>
